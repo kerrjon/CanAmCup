a6f448d79c207a52c8b8117a0fde8dc4861cdf5d
Jon Kerr <jkerr@markarch.com>
2016-03-07 14:16:28 -0600

    inital commit

---
+++ CanAmCup/CanAmCup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CanAmCup.Startup))]
+namespace CanAmCup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
